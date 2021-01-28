36a39fa7ebca75872b68f5a550ac692552b763ed
EvanLZQ <evanabyss@gmail.com>
2021-01-28 14:26:22 -0500

    Jan 28 14:26 Commit Zongqi

---
+++ TeamProjectAuction/MyEnums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProjectAuction
+{
+    public static class MyEnums
+    {
+        public enum PreferredPaymentType
+        {
+            Deposit, Cheque, CreditCard
+        }
+
+        public enum SexEnum { Male, Female }
+    }
+}
