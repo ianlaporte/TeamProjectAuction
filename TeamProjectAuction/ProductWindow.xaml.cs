7195cbf1232d136fb335806ccbd299d05b47a158
ianlaporte <ianlaporte@gmail.com>
2021-01-29 11:26:30 -0500

    Zongqi version from 09h55 to Zongqi branch remote at 11h26am le29

--- TeamProjectAuction/ProductWindow.xaml.cs
+++ TeamProjectAuction/ProductWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace TeamProjectAuction
 {
@@ -19,86 +21,243 @@ namespace TeamProjectAuction
     /// </summary>
     public partial class ProductWindow : Window
     {
+        public string filename;
+        private byte[] productImage;
+        private Client _targetClient = new Client();
+        private HashSet<Product> ProductWindowProducts = new HashSet<Product>();
+        private Random rand = new Random();
+
         public ProductWindow()
         {
             InitializeComponent();
         }
 
-    //    private void LoadData()
-    //    {
-    //        txtOwnerId.Text = "";
-    //        txtOwnerName.Text = "";
-    //        txtLotId.Text = "";
-    //        txtCategoryId.Text = "";
-    //        txtProductId.Text = "";
-    //        txtOwnerName.Text = "";
-    //        txtProductName.Text = "";
-    //        txtProductDescription.Text = "";
-    //        txtProductStartPrice.Text = "";
-    //        txtProductSoldPrice.Text = "";
+        public ProductWindow(Client ClientFromRegistration)
+        {
+            _targetClient = ClientFromRegistration;
+            InitializeComponent();
+            cbCategory.ItemsSource = Enum.GetValues(typeof(MyEnums.ProductCategory));
+            LoadProductWindow();
+        }
 
-            List<ProductOwner> productOwners = Globals.AuctionContext.ProductOwner.ToList();  //Correct this line
-            lvProductOwners.ItemsSource = productOwners;
+        public void LoadProductWindow()
+        {
+            txtOwnerId.Text = _targetClient.ClientId.ToString();
+            txtOwnerName.Text = _targetClient.ClientName;
+
+            //foreach (Product tempProduct in Globals.AuctionContext.Products)
+            //{
+            //    if (tempProduct.ClientId == _targetClient.ClientId)
+            //    {
+            //        ProductWindowProducts.Add(tempProduct);
+            //    }
+            //}
+
+            //lvProductOwners.ItemsSource = ProductWindowProducts;
+            lvProductOwners.ItemsSource = Globals.AuctionContext.Products.ToList();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            int lotId = rand.Next(1, 15);
+            int OwnerId = -1;
+
+            if (!Int32.TryParse(txtOwnerId.Text, out OwnerId))
+            {
+                MessageBox.Show("Owner Id entered invalid.", "Owner Id warning");
+                return;
+            }
+
+            LotType targetLot =
+                (from l in Globals.AuctionContext.Lots where l.LotId == lotId select l).FirstOrDefault();
+
+            if (targetLot == null)
+            {
+                MessageBox.Show("Adding failed, cannot find a proper lot.", "Lot not found warning");
+                return;
+            }
+
+            Product aProduct = new Product
+            {
+                CategoryName = (MyEnums.ProductCategory) cbCategory.SelectedItem,
+                ProductName = txtProductName.Text,
+                ProductDescription = txtProductDescription.Text,
+                ProductStartPrice = double.Parse(txtProductStartPrice.Text),
+                LotTypeId = targetLot.LotId,
+                LotType = targetLot
+            };
+
+            aProduct.ProductImage = productImage;
+            aProduct.ClientId = OwnerId;
+
+            Client TargetClient = (from c in Globals.AuctionContext.Clients
+                where c.ClientId == OwnerId
+                select c).FirstOrDefault();
+
+            aProduct.Client = TargetClient;
+
+            Globals.AuctionContext.Products.Add(aProduct);
+            Globals.AuctionContext.SaveChanges();
+
+            LoadProductWindow();
             lvProductOwners.Items.Refresh();
+            ClearFields();
+        }
 
-    //    }
+        private void btnImageUpcoming_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg;*.jpeg;*.gif;*.png)|*.jpg;*.jpeg;*.gif;*.png|All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
 
-    //    private void btnAdd_Click(object sender, RoutedEventArgs e)
-    //    {
-    //        //Validate the data
-    //        if (!ValidateData())
-    //        {
-    //            MessageBox.Show("the fields are not set properly");
-    //            return;
-    //        }
+            if (dlg.ShowDialog() == true)
+            {
+                try
+                {
+                    productImage = File.ReadAllBytes(dlg.FileName);
+                    tbImageUpcoming.Visibility = Visibility.Hidden;
+                    BitmapImage bitmap = Utils.ByteArrayToBitmapImage(productImage);
+                    imageViewerUpcoming.Source = bitmap;
 
-    //    }
+                }
+                catch (Exception ex) when (ex is SystemException || ex is IOException)
+                {
+                    MessageBox.Show(ex.Message, "File reading failed", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
 
-    //    public bool ValidateData()
-    //    {
-    //        return true;
-    //    }
 
-    //    private void lvProductOwners_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    //    {
+        private void BtnClearFields_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+        }
 
-    //    }
+        private void ClearFields()
+        {
+            txtLotId.Text = "";
+            cbCategory.SelectedIndex = -1;
+            txtProductId.Text = "";
+            txtProductName.Text = "";
+            txtProductDescription.Text = "";
+            txtProductStartPrice.Text = "";
+            txtProductSoldPrice.Text = "";
+            lvProductOwners.UnselectAll();
+            imageViewerUpcoming.Source = null;
+            tbImageUpcoming.Visibility = Visibility.Visible;
+        }
 
-    //    private void btnImageUpcoming_Click(object sender, RoutedEventArgs e)
-    //    {
+        private void ChbLotId_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (chbLotId.IsChecked == true)
+            {
+                txtLotId.IsEnabled = true;
+            }
+            else
+            {
+                txtLotId.IsEnabled = false;
+            }
+        }
 
-    //    }
+        private void ChbProductId_OnClick(object sender, RoutedEventArgs e)
+        {
 
-    //    private void btnAddManyProducts_Click(object sender, RoutedEventArgs e)
-    //    {
+            if (chbProductId.IsEnabled == true)
+            {
+                txtProductId.IsEnabled = true;
+            }
+            else
+            {
+                txtProductId.IsEnabled = false;
+            }
+        }
 
-    //    }
+        private void LvProductOwners_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvProductOwners.SelectedIndex == -1)
+            {
+                ClearFields();
+            }
+            else
+            {
+                btnUpdateProduct.IsEnabled = true;
+                btnDeleteProduct.IsEnabled = true;
+                Product TargetProduct = (Product) lvProductOwners.SelectedItem;
+                txtLotId.Text = TargetProduct.LotTypeId.ToString();
+                txtProductId.Text = TargetProduct.ProductId.ToString();
+                txtProductDescription.Text = TargetProduct.ProductDescription;
+                int selectedCategoryEnum = (int) TargetProduct.CategoryName;
+                cbCategory.SelectedIndex = selectedCategoryEnum;
+                txtProductStartPrice.Text = TargetProduct.ProductStartPrice.ToString();
+                txtProductSoldPrice.Text = TargetProduct.ProductSoldPrice.ToString();
+                txtProductName.Text = TargetProduct.ProductName;
+                BitmapImage bitmap = Utils.ByteArrayToBitmapImage(TargetProduct.ProductImage);
+                imageViewerUpcoming.Source = bitmap;
+                tbImageUpcoming.Visibility = Visibility.Hidden;
+            }
+        }
 
-    //    private void btnSearchProduct_Click(object sender, RoutedEventArgs e)
-    //    {
+        private void BtnUpdateProduct_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (lvProductOwners.SelectedIndex == -1)
+            {
+                ClearFields();
+            }
+            else
+            {
+                Product TargetProduct = (Product) lvProductOwners.SelectedItem;
+                TargetProduct.ProductImage = productImage;
+                TargetProduct.CategoryName = (MyEnums.ProductCategory) cbCategory.SelectedItem;
+                TargetProduct.ProductName = txtProductName.Text;
+                TargetProduct.ProductDescription = txtProductDescription.Text;
+                TargetProduct.ProductStartPrice = double.Parse(txtProductStartPrice.Text);
+                TargetProduct.ProductSoldPrice = double.Parse(txtProductSoldPrice.Text);
+                Globals.AuctionContext.SaveChanges();
+                ClearFields();
+                lvProductOwners.Items.Refresh();
+                MessageBox.Show("Updated!", "Update Message");
+            }
+        }
 
-    //    }
+        private void BtnDeleteProduct_OnClick(object sender, RoutedEventArgs e)
+        {
+            Product TargetProduct = (Product)lvProductOwners.SelectedItem;
+            Globals.AuctionContext.Products.Remove(TargetProduct);
+            Globals.AuctionContext.SaveChanges();
+            LoadProductWindow();
+            lvProductOwners.Items.Refresh();
+        }
 
-    //    private void btnSeeNextProduct_Click(object sender, RoutedEventArgs e)
-    //    {
+        private void BtnSearchProduct_OnClick(object sender, RoutedEventArgs e)
+        {
+            int lotNumber;
+            int productNumber;
+            if (chbLotId.IsChecked == true)
+            {
+                if (txtLotId.Text.Length == 0)
+                {
+                    MessageBox.Show("Lot Id cannot be empty! Or un-select lot id checkbox!", "Search Field Empty!");
+                    return;
+                }
 
-    //    }
+                if (! Int32.TryParse(txtLotId.Text, out lotNumber))
+                {
+                    MessageBox.Show("Lot id parsing failed!");
+                }
 
-    //    private void btnUpdateProduct_Click(object sender, RoutedEventArgs e)
-    //    {
+                List<Product> TargetProductList = new List<Product>();
 
-    //    }
+                foreach (Product tempProduct in Globals.AuctionContext.Products)
+                {
+                    if (tempProduct.LotTypeId == lotNumber)
+                    {
+                        TargetProductList.Add(tempProduct);
+                    }
+                }
 
-    //    private void btnDeleteOneProduct_Click(object sender, RoutedEventArgs e)
-    //    {
-
-    //    }
-
-    //    private void btnManyProducts_Click(object sender, RoutedEventArgs e)
-    //    {
-
-    //    }
+                lvProductOwners.ItemsSource = TargetProductList;
+                lvProductOwners.Items.Refresh();
+            }
+        }
     }
-
-
 }
